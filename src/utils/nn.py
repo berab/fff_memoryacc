e54c11aa1486fb93220f19f316e6ed406b551398
berab <beran.kilic@outlook.com>
2026-07-14 14:04:49 +0200

    dist reg fix

--- src/utils/nn.py
+++ src/utils/nn.py
@@ -4,7 +4,7 @@ from utils.prob import get_exp, get_halfnormal
 
 # TODO: Loss for maximizing sample entropy or minimizing class entropy
 def train_epoch(model, optim, loader, criterion, epoch, device, reg_alpha: float = 0.0, 
-                entropy_alpha: float = 0.0, dist_reg = None, dist_alpha = 0.0, n_mem1 = None):
+                entropy_alpha: float = 0.0, dist_reg = None, dist_alpha = 0.0, n_mem1 = 1):
     model.train()
     correct, dist_loss, running_loss, running_reg_loss, running_entropy_loss = 0, 0.0, 0.0, 0.0, 0.0
     n_leaves = model.n_leaves
@@ -12,6 +12,7 @@ def train_epoch(model, optim, loader, criterion, epoch, device, reg_alpha: float
         prob_dist = get_exp(n_mem1, n_leaves)
     elif dist_reg == "halfnormal":
         prob_dist = get_halfnormal(n_mem1, n_leaves)
+        prod_dist = prod_dist.to(device)
 
     for i, (inputs, targets) in tqdm(enumerate(loader), total=len(loader)):
         inputs, targets = inputs.to(device), targets.to(device)
